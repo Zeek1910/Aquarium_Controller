6754faba2ddd232143338572904e2e03185f49ca
Evheniy <zeek1910@gmail.com>
2021-10-07 12:10:08 +0300

    replaced library to async

--- Aquarium_Controller.ino
+++ Aquarium_Controller.ino
@@ -1,10 +1,16 @@
 #include <ESP8266WiFi.h>
-#include <WiFiManager.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPAsyncWiFiManager.h>
+
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+
 #include <TimeLib.h>
 #include <DS1307RTC.h>
 #include <TimeAlarms.h>
+
+
 
 #define RTC_GND_PIN D4
 #define RTC_VCC_PIN D3
@@ -13,31 +19,49 @@
 #define RELAY_2_PIN D1
 #define PWM_PIN D2
 
-const long utcOffsetInSeconds = 3 * 3600;
 
+
+const long utcOffsetInSeconds = 3 * 3600;
 // Auxiliar variables to store the current output state
 String relay1State = "off";
 String relay2State = "off";
 
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0;
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
-
-// Variable to store the HTTP request
-String header;
-
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 
-WiFiManager wifiManager;
-WiFiServer server(80);
+AsyncWebServer server(80);
+DNSServer dns;
+AsyncWiFiManager wifiManager(&server,&dns);
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+
+
+const char* PARAM_INPUT_1 = "input1";
+const char* PARAM_INPUT_2 = "input2";
+const char* PARAM_INPUT_3 = "input3";
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>ESP Input Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <form action="/get">
+    input1: <input type="text" name="input1">
+    <input type="submit" value="Submit">
+  </form><br>
+  <form action="/get">
+    input2: <input type="text" name="input2">
+    <input type="submit" value="Submit">
+  </form><br>
+  <form action="/get">
+    input3: <input type="text" name="input3">
+    <input type="submit" value="Submit">
+  </form>
+</body></html>)rawliteral";
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -48,110 +72,61 @@ void setup() {
   digitalWrite(RTC_VCC_PIN, HIGH);
 
   //wifiManager.resetSettings();
-  wifiManager.setConfigPortalBlocking(false);
   wifiManager.autoConnect("Aquarium");
-  server.begin();
   timeClient.begin();
   setSyncProvider(timeSyncProvider);
   if (timeStatus() != timeSet) {
     Serial.println("Unable to sync time!");
   }
+
+  initAsyncWebServer();
 }
 
 void loop() {
-  wifiManager.process();
-  WiFiClient client = server.available();
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /relay1/on") >= 0) {
-              turnOnRelay1();
-            } else if (header.indexOf("GET /relay1/off") >= 0) {
-              turnOffRelay1();
-            } else if (header.indexOf("GET /relay2/on") >= 0) {
-              turnOnRelay2();
-            } else if (header.indexOf("GET /relay2/off") >= 0) {
-              turnOffRelay2();
-            }
-
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
-
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Aquarium Web Server</h1>");
-            client.println("<h1>Today - " + curentDateToString() + "</h1>");
-            client.println("<h1>Time - " + curentTimeToString() + "</h1>");
-
-            // Display current state, and ON/OFF buttons for Relay 1
-            client.println("<p>Relay 1 - State " + relay1State + "</p>");
-            if (relay1State == "off") {
-              client.println("<p><a href=\"/relay1/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/relay1/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-            // Display current state, and ON/OFF buttons for Relay 2
-            client.println("<p>Relay 2 - State " + relay2State + "</p>");
-            if (relay2State == "off") {
-              client.println("<p><a href=\"/relay2/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/relay2/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-            client.println("<label for=\"appt\">Choose a time for your meeting:</label>");
-            client.println("<input type=\"time\" id=\"appt\" name=\"appt\"required>");
-            
-            client.println("</body></html>");
-
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
-  //Main Loop
+  Serial.println("Main Loop");
   Alarm.delay(1);
+}
+
+void initAsyncWebServer() {
+  // Send web page with input fields to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send_P(200, "text/html", index_html);
+  });
+
+  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest * request) {
+    String inputMessage;
+    String inputParam;
+    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+    }
+    // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_2)) {
+      inputMessage = request->getParam(PARAM_INPUT_2)->value();
+      inputParam = PARAM_INPUT_2;
+    }
+    // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_3)) {
+      inputMessage = request->getParam(PARAM_INPUT_3)->value();
+      inputParam = PARAM_INPUT_3;
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field ("
+                  + inputParam + ") with value: " + inputMessage +
+                  "<br><a href=\"/\">Return to Home Page</a>");
+  });
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
 }
 
 time_t timeSyncProvider() {
@@ -172,22 +147,22 @@ String curentDateToString() {
   return String(day()) + "." + String(month()) + "." + String(year());
 }
 
-void turnOffRelay1(){
+void turnOffRelay1() {
   digitalWrite(RELAY_1_PIN, HIGH);
   relay1State = "off";
 }
 
-void turnOnRelay1(){
+void turnOnRelay1() {
   digitalWrite(RELAY_1_PIN, LOW);
   relay1State = "on";
 }
 
-void turnOffRelay2(){
+void turnOffRelay2() {
   digitalWrite(RELAY_2_PIN, HIGH);
   relay2State = "off";
 }
 
-void turnOnRelay2(){
+void turnOnRelay2() {
   digitalWrite(RELAY_2_PIN, LOW);
   relay2State = "on";
 }
